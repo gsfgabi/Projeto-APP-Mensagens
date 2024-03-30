2f5d95f66d1613985ee2e1c1f0113119b780b08d
gsfgabi <gsf.gabriellasantos@gmail.com>
2024-03-30 16:10:25 -0300

    criando paginas e atualizando

---
+++ app_unichat/lib/app_widget.dart
@@ -0,0 +1,35 @@
+import 'app_controller.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+import 'register_student.dart';
+import 'register_teacher.dart';
+import 'chat_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                primarySwatch: Colors.amber,
+                brightness: AppController.instance.darkTheme
+                    ? Brightness.dark
+                    : Brightness.light),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => LoginPage(),
+              '/home': (context) => HomePage(),
+              '/chat': (context) => PaginaDeChats(),
+              '/RegisterStudent': (context) => RegisterStudent(),
+              '/RegisterTeacher': (context) => RegisterTeacher(),
+            },
+          );
+        });
+  }
+}
---
+++ app_unichat/lib/register_student.dart
@@ -0,0 +1,140 @@
+//import 'home_page.dart';
+import 'package:flutter/material.dart';
+
+class RegisterStudent extends StatefulWidget {
+  const RegisterStudent({super.key});
+
+  @override
+  State<RegisterStudent> createState() => _RegisterStudentState();
+}
+
+class _RegisterStudentState extends State<RegisterStudent> {
+  String nomecompleto = '';
+  String email = '';
+  String senha = '';
+  String selecionarcurso = '';
+  String codigoturma = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 100,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Cadastro do Aluno',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      nomecompleto = text;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Nome Completo',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      senha = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      selecionarcurso = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Selecionar Curso',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    onChanged: (text) {
+                      codigoturma = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Codigo da Turma',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF4B9460)
+                      // Text Color (Foreground color)
+                      ),
+                  child: const Text(
+                    'Cadastrar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    if (email == email && senha == senha) {
+                      Navigator.of(context).pushReplacementNamed('/');
+                      // Navigator.of(context).pushReplacement(
+                      //   MaterialPageRoute(builder: (context) => HomePage()),
+                      // );
+                    } else {
+                      print('Login Invalido');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
