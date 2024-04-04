4d3b4d6ce35b2bb1fbc5c06354a2c87af815ea57
gsfgabi <gsf.gabriellasantos@gmail.com>
2024-04-04 13:50:18 -0300

    ajuste form

--- app_unichat/lib/login_page.dart
+++ app_unichat/lib/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
+  final _chaveForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,139 +23,147 @@ class _LoginPageState extends State<LoginPage> {
             height: 100,
             child: Image.asset('assets/images/logo.png'),
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                  ),
-                ),
-                Container(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: TextFormField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    textCapitalization: TextCapitalization.none,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (valor) {
-                      if (valor == null ||
-                          valor.trim().isEmpty ||
-                          !valor.contains('@')) {
-                        return 'Por favor, insira um endereço de email válido!';
-                      }
-                      return null;
-                    },
-                    onSaved: (valorDigitado) {
-                      if (valorDigitado != null) {
-                        email = valorDigitado;
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: TextFormField(
-                    onChanged: (text) {
-                      senha = text;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (valor) {
-                      if (valor == null || valor.trim().length < 6) {
-                        return 'A senha deve ter pelo menos 6 caracteres.';
-                      }
-                      return null;
-                    },
-                    onSaved: (valor) {
-                      if (valor != null) {
-                        senha = valor;
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF4B9460),
-                    // Cor de Texto (Cor de primeiro plano)
-                  ),
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    if (email.toLowerCase() == 'admin' &&
-                        senha.toLowerCase() == 'admin') {
-                      // Redireciona para a criação de uma conta como professor
-                      Navigator.of(context).pushNamed('/RegisterTeacher');
-                    } else {
-                      // Verifica se o email e a senha correspondem a algum critério
-                      if (email.isNotEmpty && senha.isNotEmpty) {
-                        // Verifica se o email é válido
-                        if (email.contains('@')) {
-                          // Se todas as condições forem atendidas, redireciona para a tela de chat
-                          Navigator.of(context).pushReplacementNamed('/chat');
-                        } else {
-                          // Exibe uma mensagem de erro se o email for inválido
-                          print(
-                              'Por favor, insira um endereço de email válido!');
-                        }
-                      } else {
-                        // Exibe uma mensagem de erro se o email ou senha estiverem vazios
-                        print('Por favor, insira seu email e senha!');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+          Card(
+            // width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height,
+            margin: const EdgeInsets.all(20),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _chaveForm,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    GestureDetector(
-                      onTap: () {
-                        // Navegar para a página "Esqueci a Senha" aqui
-                        Navigator.pushNamed(context, '/esqueciSenha');
-                      },
-                      child: Text(
-                        'Esqueci a Senha',
-                        style: TextStyle(
-                            decoration: TextDecoration.none,
-                            color: Color(0xFF4B9460)),
+                    const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 30.0,
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        // Navegar para a página "Primeiro Acesso" aqui
-                        Navigator.pushNamed(context, '/RegisterStudent');
-                      },
-                      child: Text(
-                        'Primeiro Acesso',
-                        style: TextStyle(
-                            decoration: TextDecoration.none,
-                            color: Color(0xFF4B9460)),
+                    Container(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (valor) {
+                          if (valor == null ||
+                              valor.trim().isEmpty ||
+                              !valor.contains('@')) {
+                            return 'Por favor, insira um endereço de email válido!';
+                          }
+                          return null;
+                        },
+                        onSaved: (valorDigitado) {
+                          if (valorDigitado != null) {
+                            email = valorDigitado;
+                          }
+                        },
                       ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          senha = text;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (valor) {
+                          if (valor == null || valor.trim().length < 6) {
+                            return 'A senha deve ter pelo menos 6 caracteres.';
+                          }
+                          return null;
+                        },
+                        onSaved: (valor) {
+                          if (valor != null) {
+                            senha = valor;
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF4B9460),
+                        // Cor de Texto (Cor de primeiro plano)
+                      ),
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        if (email.toLowerCase() == 'admin' &&
+                            senha.toLowerCase() == 'admin') {
+                          // Redireciona para a criação de uma conta como professor
+                          Navigator.of(context).pushNamed('/RegisterTeacher');
+                        } else {
+                          // Verifica se o email e a senha correspondem a algum critério
+                          if (email.isNotEmpty && senha.isNotEmpty) {
+                            // Verifica se o email é válido
+                            if (email.contains('@')) {
+                              // Se todas as condições forem atendidas, redireciona para a tela de chat
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/chat');
+                            } else {
+                              // Exibe uma mensagem de erro se o email for inválido
+                              print(
+                                  'Por favor, insira um endereço de email válido!');
+                            }
+                          } else {
+                            // Exibe uma mensagem de erro se o email ou senha estiverem vazios
+                            print('Por favor, insira seu email e senha!');
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // Navegar para a página "Esqueci a Senha" aqui
+                            Navigator.pushNamed(context, '/esqueciSenha');
+                          },
+                          child: Text(
+                            'Esqueci a Senha',
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Color(0xFF4B9460)),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // Navegar para a página "Primeiro Acesso" aqui
+                            Navigator.pushNamed(context, '/RegisterStudent');
+                          },
+                          child: Text(
+                            'Primeiro Acesso',
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Color(0xFF4B9460)),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
