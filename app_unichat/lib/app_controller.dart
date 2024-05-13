9a493c034a12e5a281dc0c1e145091abef732579
gsfgabi <gsf.gabriellasantos@gmail.com>
2024-05-13 18:24:23 -0300

    ajustes

--- app_unichat/lib/app_controller.dart
+++ app_unichat/lib/app_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppController extends ChangeNotifier {
   static AppController instance = AppController();
 
+
   bool darkTheme = false;
   changeTheme() {
     darkTheme = !darkTheme;
