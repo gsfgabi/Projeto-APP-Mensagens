4f552d92b89f5f4226223416325d144c81c2f3b9
Kethellin <kethellinpereira2018@outlook.com>
2024-05-14 22:00:49 -0300

    ajustes 14-05

--- app_unichat/lib/login_page.dart
+++ app_unichat/lib/login_page.dart
@@ -19,120 +19,62 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                width: 300,
-                height: 100,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              Card(
-                // width: MediaQuery.of(context).size.width,
-                // height: MediaQuery.of(context).size.height,
-                margin: const EdgeInsets.all(20),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    key: _chaveForm,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 30.0,
-                          ),
+        body: SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 300,
+              height: 100,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            Card(
+              // width: MediaQuery.of(context).size.width,
+              // height: MediaQuery.of(context).size.height,
+              margin: const EdgeInsets.all(20),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _chaveForm,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 30.0,
                         ),
-                        Container(
-                          height: 20,
-                        ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: TextFormField(
-                        onChanged: (text) {
-                          _senha = text;
-                        },
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: 'Senha',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (valor) {
-                          if (valor == null || valor.trim().length < 6) {
-                            return 'A senha deve ter pelo menos 6 caracteres.';
-                          }
-                          return null;
-                        },
-                        onSaved: (valor) {
-                          if (valor != null) {
-                            _senha = valor;
-                          }
-                        },
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF4B9460),
-                          // Cor de Texto (Cor de primeiro plano)
-                        ),
-                        child: const Text(
-                          'Entrar',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () async {
-                          if (!_chaveForm.currentState!.validate()) {
-                            return;
-                          }
-                          _chaveForm.currentState!.save();
-
-                          // Imprimir no console o nome de usuário e senha
-                          print('Email do Usuário: $_email');
-                          print('Senha: $_senha');
-
-                          try {
-                            if (_email.toLowerCase() == 'admin' &&
-                              _senha.toLowerCase() == 'admin') {
-                              // Redireciona para a criação de uma conta como professor
-                              Navigator.of(context).pushNamed('/RegisterTeacher');
-                            } else {
-                              // Verifica se o email e a senha foram fornecidos
-                              if (_email.isNotEmpty && _senha.isNotEmpty) {
-                                // Verifica se o email é válido
-                                if (_email.contains('@unicv.edu.br')) {
-                                  // Se todas as condições forem atendidas, redireciona para a tela de chat
-                                  await _firebaseAuth
-                                  .signInWithEmailAndPassword(
-                                      email: _email,
-                                      password: _senha);
-                                  Navigator.of(context)
-                                    .pushReplacementNamed('/chat');
-                                }
-                              }else {
-                              // Exibe uma mensagem de erro se o email ou senha estiverem vazios
-                              print('Por favor, insira seu email e senha!');
-                              }
+                      Container(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: TextFormField(
+                          onChanged: (text) {
+                            _email = text;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (valor) {
+                            if (valor == null ||
+                                valor.trim().isEmpty ||
+                                !valor.contains('@unicv.edu.br')) {
+                              return 'Por favor, insira um endereço de email válido!';
                             }
-                          } on FirebaseAuthException catch (error) {
-                            String mensagem =
-                                'Falha no Registro de novo Usuário';
-                            if (error.code ==
-                                'email-already-in-use') {
-                              mensagem = 'Email já utilizado';
+                            return null;
+                          },
+                          onSaved: (valorDigitado) {
+                            if (valorDigitado != null) {
+                              _email = valorDigitado;
                             }
-                            ScaffoldMessenger.of(context)
-                                .clearSnackBars();
-                            ScaffoldMessenger.of(context)
-                              .showSnackBar(
-                              SnackBar(
-                                content: Text(mensagem),
-                              ),
-                            );
-                          }
-                        },
+                          },
+                        ),
                       ),
                       const SizedBox(height: 15),
                       Padding(
@@ -170,7 +112,7 @@ class _LoginPageState extends State<LoginPage> {
                             'Entrar',
                             style: TextStyle(color: Colors.white),
                           ),
-                          onPressed: () {
+                          onPressed: () async {
                             if (!_chaveForm.currentState!.validate()) {
                               return;
                             }
@@ -180,28 +122,45 @@ class _LoginPageState extends State<LoginPage> {
                             print('Email do Usuário: $_email');
                             print('Senha: $_senha');
         
-                            if (_email.toLowerCase() == 'admin' &&
+                            try {
+                              if (_email.toLowerCase() == 'admin' &&
                                 _senha.toLowerCase() == 'admin') {
-                              // Redireciona para a criação de uma conta como professor
-                              Navigator.of(context).pushNamed('/RegisterTeacher');
-                            } else {
-                              // Verifica se o email e a senha foram fornecidos
-                              if (_email.isNotEmpty && _senha.isNotEmpty) {
-                                // Verifica se o email é válido
-                                if (_email.contains('@unicv.edu.br')) {
-                                  // Se todas as condições forem atendidas, redireciona para a tela de chat
-                                  Navigator.of(context)
-                                      .pushReplacementNamed('/chat');
-                                } else {
-                                  // Exibe uma mensagem de erro se o email for inválido
-                                  print(
-                                      'Por favor, insira um endereço de email válido!');
-                                }
+                                // Redireciona para a criação de uma conta como professor
+                                Navigator.of(context).pushNamed('/RegisterTeacher');
                               } else {
+                                // Verifica se o email e a senha foram fornecidos
+                                if (_email.isNotEmpty && _senha.isNotEmpty) {
+                                  // Verifica se o email é válido
+                                  if (_email.contains('@unicv.edu.br')) {
+                                    // Se todas as condições forem atendidas, redireciona para a tela de chat
+                                    await _firebaseAuth
+                                    .signInWithEmailAndPassword(
+                                        email: _email,
+                                        password: _senha);
+                                    Navigator.of(context)
+                                      .pushReplacementNamed('/chat');
+                                  }
+                                }else {
                                 // Exibe uma mensagem de erro se o email ou senha estiverem vazios
                                 print('Por favor, insira seu email e senha!');
+                                }
                               }
-                            }
+                            } on FirebaseAuthException catch (error) {
+                              String mensagem =
+                                  'Falha no Registro de novo Usuário';
+                              if (error.code ==
+                                  'email-already-in-use') {
+                                mensagem = 'Email já utilizado';
+                              }
+                              ScaffoldMessenger.of(context)
+                                  .clearSnackBars();
+                              ScaffoldMessenger.of(context)
+                                .showSnackBar(
+                                SnackBar(
+                                  content: Text(mensagem),
+                                ),
+                              );
+                            }                    
                           },
                         ),
                       ]),
@@ -239,7 +198,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
