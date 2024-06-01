66075aecb720cbdc1dd95e637255f0035d2e8781
unknown <wendel-vinicius@live.com>
2024-06-01 03:17:37 -0300

    Ajuste tela de cadastro do aluno - Seleção de curso

--- app_unichat/lib/register_student.dart
+++ app_unichat/lib/register_student.dart
@@ -1,4 +1,3 @@
-//import 'home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 final _firebaseAuth = FirebaseAuth.instance;
 
 class RegisterStudent extends StatefulWidget {
-  const RegisterStudent({super.key});
+  const RegisterStudent({Key? key});
 
   @override
   State<RegisterStudent> createState() => _RegisterStudentState();
@@ -16,218 +15,227 @@ class _RegisterStudentState extends State<RegisterStudent> {
   String nomecompleto = '';
   String email = '';
   String senha = '';
-  String selecionarcurso = '';
+  String? selecionarcurso; // Alterado para String nullable
   String codigoturma = '';
   final _chaveForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            width: 300,
-            height: 100,
-            child: Image.asset('assets/images/logo.png'),
-          ),
-          
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Form(
-              key: _chaveForm,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Cadastro do Aluno',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  Container(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        nomecompleto = text;
-                      },
-                      decoration: const InputDecoration(
-                        labelText: 'Nome Completo',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (valor) {
-                        if (valor == null ||
-                            valor.trim().isEmpty ) {
-                          return 'Por favor, insira um nome válido!';
-                        }
-                        return null;
-                      },
-                      onSaved: (valorDigitado) {
-                        if (valorDigitado != null) {
-                          nomecompleto = valorDigitado;
-                        }
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        email = text;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (valor) {
-                        if (valor == null ||
-                            valor.trim().isEmpty ||
-                            !valor.contains('@unicv.edu.br')) {
-                          return 'Por favor, insira um endereço de email válido!';
-                        }
-                        return null;
-                      },
-                      onSaved: (valorDigitado) {
-                        if (valorDigitado != null) {
-                          email = valorDigitado;
-                        }
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        senha = text;
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: 'Senha',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (valor) {
-                        if (valor == null || valor.trim().length < 6) {
-                          return 'A senha deve ter pelo menos 6 caracteres.';
-                        }
-                        return null;
-                      },
-                      onSaved: (valor) {
-                        if (valor != null) {
-                          senha = valor;
-                        }
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        selecionarcurso = text;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: 'Selecionar Curso',
-                        border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 300,
+              height: 100,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Form(
+                key: _chaveForm,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Cadastro do Aluno',
+                      style: TextStyle(
+                        fontSize: 30.0,
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        codigoturma = text;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: 'Codigo da Turma',
-                        border: OutlineInputBorder(),
-                      ),
+                    Container(
+                      height: 20,
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF4B9460)
-                        // Text Color (Foreground color)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          nomecompleto = text;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Nome Completo',
+                          border: OutlineInputBorder(),
                         ),
-                    child: const Text(
-                      'Cadastrar',
-                      style: TextStyle(color: Colors.white),
+                        validator: (valor) {
+                          if (valor == null || valor.trim().isEmpty) {
+                            return 'Por favor, insira um nome válido!';
+                          }
+                          return null;
+                        },
+                        onSaved: (valorDigitado) {
+                          if (valorDigitado != null) {
+                            nomecompleto = valorDigitado;
+                          }
+                        },
+                      ),
                     ),
-                    onPressed: () async {
-                      if (!_chaveForm.currentState!.validate()) {
-                        return;
-                      }
-                      _chaveForm.currentState!.save();
-
-                      if (email == email && senha == senha) {
-                        try {
-                          if (email.toLowerCase() == 'admin' &&
-                            senha.toLowerCase() == 'admin') {
-                            // Redireciona para a criação de uma conta como professor
-                            Navigator.of(context).pushNamed('/RegisterTeacher');
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (valor) {
+                          if (valor == null ||
+                              valor.trim().isEmpty ||
+                              !valor.contains('@unicv.edu.br')) {
+                            return 'Por favor, insira um endereço de email válido!';
+                          }
+                          return null;
+                        },
+                        onSaved: (valorDigitado) {
+                          if (valorDigitado != null) {
+                            email = valorDigitado;
+                          }
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          senha = text;
+                        },
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (valor) {
+                          if (valor == null || valor.trim().length < 6) {
+                            return 'A senha deve ter pelo menos 6 caracteres.';
+                          }
+                          return null;
+                        },
+                        onSaved: (valor) {
+                          if (valor != null) {
+                            senha = valor;
+                          }
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance
+                            .collection('salas-participantes')
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return const CircularProgressIndicator();
                           } else {
-                            // Verifica se o email e a senha foram fornecidos
-                            if (email.isNotEmpty && senha.isNotEmpty) {
-                              // Verifica se o email é válido
-                              if (email.contains('@unicv.edu.br')) {
-                                // Se todas as condições forem atendidas, redireciona para a tela de chat
-                                final credenciaisUsuario =
-                                              await _firebaseAuth
-                                                  .createUserWithEmailAndPassword(
-                                                      email: email,
-                                                      password: senha);
-
-                                                    await FirebaseFirestore.instance.collection('usuarios').doc(credenciaisUsuario.user!.uid).set({
-                                                      'email' : email,
-                                                      'isAdmin': false,
-                                                      'usuario': nomecompleto,
-                                                    });
-                                Navigator.of(context)
-                                  .pushReplacementNamed('/turma');
+                            List<String> cursos = [];
+                            for (var doc in snapshot.data!.docs) {
+                              var data = doc.data() as Map<String, dynamic>;
+                              if (data.containsKey('nome')) {
+                                String? nomeCurso = data['nome'];
+                                if (nomeCurso != null) {
+                                  cursos.add(nomeCurso);
+                                }
                               }
-                            }else {
-                              // Exibe uma mensagem de erro se o email ou senha estiverem vazios
-                              print('Por favor, insira seu email e senha!');
                             }
+                            return DropdownButtonFormField<String>(
+                              value: selecionarcurso,
+                              decoration: const InputDecoration(
+                                labelText: 'Selecionar Curso',
+                                border: OutlineInputBorder(),
+                              ),
+                              items: cursos.map((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  selecionarcurso = value;
+                                });
+                              },
+                              validator: (valor) {
+                                if (valor == null || valor.isEmpty) {
+                                  return 'Por favor, selecione um curso!';
+                                }
+                                return null;
+                              },
+                            );
                           }
-                        } on FirebaseAuthException catch (error) {
-                          String mensagem =
-                              'Falha no cadastro de novo Usuário';
-                          if (error.code ==
-                              'email-already-in-use') {
-                            mensagem = 'Email já utilizado';
-                          }
-                          ScaffoldMessenger.of(context)
-                              .clearSnackBars();
-                          ScaffoldMessenger.of(context)
-                            .showSnackBar(
-                            SnackBar(
-                              content: Text(mensagem),
-                            ),
-                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          codigoturma = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Codigo da Turma',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF4B9460),
+                      ),
+                      onPressed: () async {
+                        if (!_chaveForm.currentState!.validate()) {
+                          return;
                         }
-                        // Navigator.of(context).pushReplacementNamed('/RegisterStudent');
-                      
-                      } else {
-                        print('Login Invalido');
+                        _chaveForm.currentState!.save();
+
+                        if (email == 'admin' && senha == 'admin') {
+                          Navigator.of(context)
+                              .pushNamed('/RegisterTeacher');
+                        } else {
+                          if (email.isNotEmpty && senha.isNotEmpty) {
+                            if (email.contains('@unicv.edu.br')) {
+                              final credenciaisUsuario =
+                                  await _firebaseAuth
+                                      .createUserWithEmailAndPassword(
+                                email: email,
+                                password: senha,
+                              );
+
+                              await FirebaseFirestore.instance
+                                  .collection('usuarios')
+                                  .doc(credenciaisUsuario.user!.uid)
+                                  .set({
+                                'email': email,
+                                'isAdmin': false,
+                                'usuario': nomecompleto,
+                              });
+
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/turma');
+                            }
+                          } else {
+                            print('Por favor, insira seu email e senha!');
+                          }
                         }
                       },
+                      child: const Text(
+                        'Cadastrar',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                     const SizedBox(height: 15),
                     TextButton(
                       onPressed: () {
-                        // Retornar à tela de login
-                        Navigator.of(context).pushReplacementNamed('/login');
+                        Navigator.of(context)
+                            .pushReplacementNamed('/login');
                       },
                       child: const Text(
                         'Já possui uma conta? Faça login',
